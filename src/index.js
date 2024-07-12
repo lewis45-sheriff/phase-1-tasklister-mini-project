61192ac8582d67778f7ed0ac965b90524c846cdb
lewis <lewiskipkemoi53@gmail.com>
2024-07-12 10:11:19 +0300

    all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  document.querySelector("form").addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleTask(e.target.newtaskdescription.value);
+  });
 });
+
+function handleTask(todo) {
+  let taskList = document.getElementById('tasks');
+
+  let newTask = document.createElement('p');
+  newTask.textContent = todo;
+
+  let btn = document.createElement("button");
+  btn.textContent = 'x';
+  btn.addEventListener('click', handleDelete);
+
+  newTask.appendChild(btn);
+  taskList.appendChild(newTask);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
